17dfba8eff3004bc5a99d1e0628c1eda0dcb7d40
Брылунова Оксана Александровна <brylunova@kontur>
2020-01-21 20:46:25 +0500

    Решила задачу 2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,10 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  if (!name || name.length < 4) {
+    return false;
+  }
+  else {return name.indexOf(' ') === -1;}
 }
 
 function sayHello() {
@@ -21,3 +25,4 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
