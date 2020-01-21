cf52112db1bebfd95b4d939dc8ba24346e09597b
Брылунова Оксана Александровна <brylunova@kontur>
2020-01-21 21:07:59 +0500

    Решила третью задачу

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,11 @@
  * @returns {string}
  */
 function ucFirst(str) {
+  if (str === '') {
+    return '';
+  }
+  if (str.length === 1) {
+    return str.toUpperCase();
+  }
+  return str[0].toUpperCase() + str.slice(1);
 }
