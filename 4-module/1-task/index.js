da465f76b5dc79a8e50c530fdf8e94ac530bca86
Брылунова Оксана Александровна <brylunova@kontur>
2020-02-06 10:31:52 +0500

    решила задачу

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,14 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let list = document.createElement('ul');
+
+  for (let i = 0; i < friends.length; i++) {
+    let card = document.createElement('li');
+    card.textContent = friends[i].firstName + ' ' + friends[i].lastName;
+    list.appendChild(card);
+  }
+
+  return list;
 }
+
