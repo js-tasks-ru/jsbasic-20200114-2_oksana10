c6722a2036a01303eed3f4ab5251290126ca886d
Брылунова Оксана Александровна <brylunova@kontur>
2020-02-16 19:07:37 +0500

    решила задачу

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,13 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  for (let i = 0; i < table.rows.length; i++) {
+    let cells = table.rows[i].cells;
+    for (let j = 0; j < cells.length; j++) {
+      if (i === j) {
+        let element = cells[j];
+        element.style.backgroundColor = "red";
+      }
+    }
+  }
 }
